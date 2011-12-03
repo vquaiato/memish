ff5aab36bd9e8fdfced4747289ab97b3ddb2cffc
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2011-12-03 00:57:14 -0200

    memish ftw

---
+++ memish.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+convert -fill white -stroke black -strokewidth 2 -background transparent -gravity center -size 390x60 -font Impact-Normal label:"$3" $1_base.jpg +swap -gravity south -composite $1.jpg
+
+convert -fill white -stroke black -strokewidth 2 -background transparent -gravity center -size 390x60 -font Impact-Normal label:"$2" $1.jpg +swap -gravity north -composite $1.jpg
