74faca134a00cbf245a20004ad130fe0ff6e0490
Vinicius Quaiato <vinicius.quaiato@gmail.com>
2011-12-03 01:20:10 -0200

    fixing memish

--- memish.sh
+++ memish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-convert -fill white -stroke black -strokewidth 2 -background transparent -gravity center -size 390x60 -font Impact-Normal label:"$2" $1.jpg +swap -gravity north -composite $1.jpg
-convert -fill white -stroke black -strokewidth 2 -background transparent -gravity center -size 390x60 -font Impact-Normal label:"$3" $1_base.jpg +swap -gravity south -composite $1.jpg
+convert -fill white -stroke black -strokewidth 2 -background transparent -gravity center -size 390x60 -font Impact-Normal label:"$2" $1_base.jpg +swap -gravity north -composite $1.jpg
+convert -fill white -stroke black -strokewidth 2 -background transparent -gravity center -size 390x60 -font Impact-Normal label:"$3" $1.jpg +swap -gravity south -composite $1.jpg
